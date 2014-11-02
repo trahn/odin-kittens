9fc6315de97f580b41da1fc5010e7cf35d9228cc
Till Rahn <till.rahn@gmail.com>
2014-11-02 09:13:43 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OdinKittens::Application.config.secret_key_base = 'bb53563b1e7c0b4322a154f763d31d8b45abf9d7fc4c733c9ad1aecf38ae41c91bb3c36a4c5991c2c720e8d4893866b24703bfecdfffeeb11535cff40afbca22'
